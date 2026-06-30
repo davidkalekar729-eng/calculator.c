4234658f3cc17b4a671b0d220433ba53202bcdc3
davidkalekar729-eng <davidkalekar729@gmail.com>
2026-06-30 12:16:26 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Addition = %d\n", a + b);
+    printf("Subtraction = %d\n", a - b);
+    printf("Multiplication = %d\n", a * b);
+
+    if (b != 0)
+        printf("Division = %.2f\n", (float)a / b);
+    else
+        printf("Division by zero is not allowed.\n");
+
+    return 0;
+}
